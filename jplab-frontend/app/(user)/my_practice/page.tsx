98ef638cf4d0f512c77d75244eb1d451e92a19e0
a-r-akib <arifrabbaniakib@kaicomsol.com>
2025-11-16 11:46:34 +0600

    my prectice

--- jplab-frontend/app/(user)/my_practice/page.tsx
+++ jplab-frontend/app/(user)/my_practice/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect, Suspense } from "react";
+import { useRouter } from "next/navigation";
 
 import axiosInstance from "@/utils/axios";
 import { toast } from "sonner";
@@ -30,6 +31,8 @@ export default function MyPractice() {
 
   const [loading, setLoading] = useState(true);
   const [practiceData, setPracticeData] = useState<Practice[]>([]);
+
+  const router = useRouter()
 
   const modules = {
     current: {
@@ -110,31 +113,15 @@ export default function MyPractice() {
                       ></div>
                     </div>
                     <div className="flex justify-end mt-5">
-                      <button className="px-8 py-2 bg-white rounded-md font-semibold drop-shadow-sm drop-shadow-purple-400 border border-purple-400 relative overflow-clip group">
+                      <button onClick={()=>router.push(`/practice/${item.stage_slug}/${item.current_stage_id}}`)} disabled={item?.complete == 100} className="px-8 py-2 bg-white rounded-md font-semibold drop-shadow-sm drop-shadow-purple-400 border border-purple-400 relative overflow-clip group">
                         <span className="relative z-10 text-white group-hover:text-black duration-300">
-                          Resume
+                          {item.complete == 100 ? "Completed" : "Resume"}
                         </span>
                         <div className="size-full absolute top-0 left-0 bg-violet-700 group-hover:translate-x-full duration-300"></div>
                       </button>
                     </div>
                   </div>
                 ))}
-
-                {/* Next Module */}
-                {/* <div className="relative p-8 rounded-2xl bg-violet-500 border border-violet-600 shadow-[0_0_25px_rgba(255,255,255,0.1)] hover:shadow-[0_0_40px_rgba(96,165,250,0.4)] transition-all">
-                  <div className="flex items-center justify-between mb-4">
-                    <h3 className="text-2xl font-semibold flex items-center gap-2 text-white">
-                      <FaClock className="text-green-300" /> Next Practice
-                    </h3>
-                    <span className="text-sm text-white font-medium">
-                      Coming Up
-                    </span>
-                  </div>
-                  <h4 className="text-xl font-semibold mb-2 text-white">
-                    {modules.next.title}
-                  </h4>
-                  <p className="text-gray-100">{modules.next.description}</p>
-                </div> */}
               </div>
             </section>
           </div>
