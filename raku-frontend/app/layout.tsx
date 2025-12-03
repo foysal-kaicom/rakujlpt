de371000fe05e34af35238174dcad409054e51c5
a-r-akib <arifrabbaniakib@kaicomsol.com>
2025-12-03 15:47:26 +0600

    toast

--- raku-frontend/app/layout.tsx
+++ raku-frontend/app/layout.tsx
@@ -87,7 +87,7 @@ export default function RootLayout({
       <body className="max-w-[2000px] mx-auto">
         <RouteLoader />
         {children}
-        <Toaster position="top-center" richColors />
+        <Toaster position="top-center" richColors duration={3000} closeButton={true}/>
       </body>
     </html>
   );
