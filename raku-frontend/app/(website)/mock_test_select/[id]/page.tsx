69b7a2023c2e39a91e999f3919d34079b03fd41b
a-r-akib <arifrabbaniakib@kaicomsol.com>
2026-01-29 09:14:49 +0600

    mocktest details

--- raku-frontend/app/(website)/mock_test_select/[id]/page.tsx
+++ raku-frontend/app/(website)/mock_test_select/[id]/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Suspense } from "react";
 
-import MockTestQuestionComposition from "./MockTestSelect";
 import QuestionCompositionSkeleton from "../../question_composition/QusetionCompositionSkeleton";
 import TestDetailsPage from "./MocktestDetailsComponent";
 
@@ -17,7 +16,6 @@ export default function MockTestQuestionCompositionWrapper() {
       <div className="relative bg-gradient-to-br from-blue-50 via-pink-50 to-purple-100 overflow-clip">
         <div className="absolute -top-16 -left-16 w-60 h-60 bg-yellow-200/30 rounded-full filter blur-3xl animate-bounce-slow"></div>
         <div className="absolute -bottom-24 -right-16 w-96 h-96 bg-pink-200/30 rounded-full filter blur-3xl animate-pulse-slow"></div>
-        {/* <MockTestQuestionComposition /> */}
         <TestDetailsPage />
       </div>
     </Suspense>
